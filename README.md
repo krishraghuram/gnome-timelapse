7f55d3840aa24a80627b5617ff37c2f185308222
Bruno Croci <crocidb@gmail.com>
2024-04-08 21:58:31 +0200

    improved README.md

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ It seems there's no ready-to-use tool that just makes a screen timelapse in Ubun
 Install dependencies:
 
 ```shell
-sudo apt install gnome-screenshot ffmpeg sl
+sudo apt install gnome-screenshot ffmpeg
 ```
 
 Clone this repository:
@@ -19,8 +19,36 @@ git clone git@github.com:CrociDB/gnome-timelapse && cd gnome-timelapse
 Now run it with the following parameters:
 
 ```shell
-./gnome-timelapse.sh 30
+./gnome-timelapse.sh capture .5 full timelapse.mp4
 ```
 
+This will capture `.5` screens per second (aka one screen every two seconds) and will remaing capturing until you cancel it by pressing **Ctrl+C**, then it will generate the video file.
+
+## How to use
+
+```shell
+$ /timelapse.sh
+
+Gnome Timelapse v0.1
+
+How to use:
+         ./timelapse.sh COMMAND [parameters]
+
+        This will start capturing. Press Ctrl+C to top the capturing and it will generate the final timelapse file.
+
+Commands:
+        - clean         Cleans all the capture files in this directory
+        - capture       Captures the timelapse
+                 - cps: Captures per second
+                 - [full|novideo]: 'full' means it will take the captures and then produce a video in the end the 'novideo' will not produce the video and keep the captures
+                 - filename:    Name of the output file
+         - video        Makes the video of all the captures in the current directory
+
+Example:
+To capture a timelapse video of 1 frame per second, do:30
+        ./timelapse capture 1 full out.mp4
+```
 
 ## Explanation
+
+This script uses `gnome-screenshot` to take screenshots and then uses `ffmpeg` to assemble a video file with the captures. The reason there's a custom version of `gnome-screenshot` is that in the original version of that software, you get a screen flash and a shutter sound on every capture, and it would be very annoying, so I edited that out of the source and rebuilt.
--- timelapse.sh
+++ timelapse.sh
@@ -2,20 +2,18 @@ FRAMERATE=60
 
 clean()
 {
-    printf "Cleaning captures..."
+    printf "Cleaning captures...\n"
     rm -rf _timelapse*.jpg
 }
 
 generate_video()
 {
-    printf "Generating video..."
+    printf "Generating video...\n"
     ffmpeg -framerate $FRAMERATE -pattern_type glob -i '_timelapse*.jpg' -c:v libx264 $1
 }
 
 printf "Gnome Timelapse v0.1\n\n"
-if [[ "$#" -eq 0 || "$1" == "-h" || "$1" == "help" ]]; then
-    printf "How to use:\n\t ./timelapse.sh COMMAND [parameters]\n\nCommands:\n\t- clean\t\tCleans all the capture files in this directory\n\t- capture\tCaptures the timelapse\n\t\t - cps: Captures per seconds\n\t\t - [full|novideo]: 'full' means it will take the captures and then produce a video in the end the 'novideo' will not produce the video and keep the captures\n\t\t - filename:\tName of the output file\n\t - video\tMakes the video of all the captures in the current directory\n\nExample:\nTo capture a timelapse video of 1 frame per second, do:\n\t./timelapse capture 1 full out.mp4\n\n"
-elif [[ "$1" == "capture" ]]; then
+if [[ "$1" == "capture" ]]; then
     printf "Capturing timelapse...\n"
 
     cps=${2:-3}
@@ -43,7 +41,7 @@ elif [[ "$1" == "capture" ]]; then
 
     printf "\nDone\n\n"
 elif [[ "$1" == "clean" ]]; then
-    printf "Cleaning captures..."
+    printf "Cleaning captures...\n"
     rm -rf _timelapse*.jpg
 elif [[ "$1" == "video" ]]; then
     output=${2:-out.mp4}
@@ -53,5 +51,5 @@ elif [[ "$1" == "video" ]]; then
         clean
     fi
 else
-    echo "Arguments: $#"
+    printf "How to use:\n\t ./timelapse.sh COMMAND [parameters]\n\n\tThis will start capturing. Press Ctrl+C to top the capturing and it will generate the final timelapse file.\n\nCommands:\n\t- clean\t\tCleans all the capture files in this directory\n\t- capture\tCaptures the timelapse\n\t\t - cps: Captures per second\n\t\t - [full|novideo]: 'full' means it will take the captures and then produce a video in the end the 'novideo' will not produce the video and keep the captures\n\t\t - filename:\tName of the output file\n\t - video\tMakes the video of all the captures in the current directory\n\nExample:\nTo capture a timelapse video of 1 frame per second, do:\n\t./timelapse capture 1 full out.mp4\n\n"
 fi
